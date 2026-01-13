232aa146e57d9bef1062ef7472b35a5ac1fb30dd
shoaib3110999 <saiyadayaz116@gmail.com>
2026-01-13 17:50:48 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", formData);
+      const res = await axios.post("https://medical-store-backend-sztf.onrender.com/login", formData);
 
 
 
